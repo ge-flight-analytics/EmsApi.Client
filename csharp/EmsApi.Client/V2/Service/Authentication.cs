c64d9061ce18f30e2c9e7abbfe85008537bf9f52
Richard Pineo <Richard.E.Pineo@ge.com>
2017-02-23 00:46:28 -0800

    Revert "It looks like the wrong property was accessed."
    This reverts commit 0831180ac32034403646d4cc6b2b2f9d5d162a4e.

--- csharp/EmsApi.Client/V2/Service/Authentication.cs
+++ csharp/EmsApi.Client/V2/Service/Authentication.cs
@@ -113,7 +113,7 @@ namespace EmsApi.Client.V2
 
                 if( !response.IsSuccessStatusCode )
                 {
-                    string description = result.GetValue( "message" ).ToString();
+                    string description = result.GetValue( "error_description" ).ToString();
                     error = string.Format( "Unable to retrieve EMS API bearer token: {0}", description );
                     return false;
                 }
