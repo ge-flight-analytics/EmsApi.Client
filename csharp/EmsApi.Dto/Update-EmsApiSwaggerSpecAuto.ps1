7b82d060177ae18747e80a2d637c1f3e585c0e24
Cody Owens <owenscody@gmail.com>
2017-02-24 21:27:26 -0600

    Fix a typo

--- csharp/EmsApi.Dto/Update-EmsApiSwaggerSpecAuto.ps1
+++ csharp/EmsApi.Dto/Update-EmsApiSwaggerSpecAuto.ps1
@@ -26,7 +26,7 @@ try
     $passConverted = $pass | ConvertTo-SecureString -AsPlainText -Force
     Write-Host "Writing updated swagger spec to $PSScriptRoot\ems-api.json" -ForegroundColor Yellow
     & .\Get-EmsApiSwaggerSpec.ps1 -Endpoint $endpoint -UserName $user -Password $passConverted -OutFile "ems-api.json" | Out-Null
-    Write-Host "Swagger update suceeded" -ForegroundColor Yellow
+    Write-Host "Swagger update succeeded" -ForegroundColor Yellow
 }
 finally
 {
