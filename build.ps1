26a97ebdff5cc43f7bae38064bbca00aa6a2449b
Cody Owens <owenscody@gmail.com>
2018-09-24 20:12:53 -0500

    Change version to 1.1.0, change build to include a prerelease version

---
+++ build.ps1
@@ -0,0 +1,34 @@
+param(
+    [Parameter()]
+    [string] $Version,
+
+    [Parameter()]
+    [string] $Configuration
+)
+
+if( -not $Version ) {
+    if( $env:APPVEYOR_REPO_TAG ) {
+        $tag = $env:APPVEYOR_REPO_TAG_NAME
+        $tag = $tag.TrimStart( 'v' )
+        $Version = $tag
+    }
+    else {
+        if( $env:APPVEYOR_BUILD_VERSION ) { 
+            $baseVersion = $env:APPVEYOR_BUILD_VERSION 
+        }
+        else {
+            $baseVersion = (Get-Content "$PSScriptRoot/appveyor.yml" | Select-Object -First 1).Replace( 'version: ', '' )
+        }
+
+        $b = $env:APPVEYOR_BUILD_NUMBER
+        $stamp = (Get-Date).ToString( 'yyyyMMddTHHmmssfff' )
+        $Version = "$baseVersion-rc$stamp$b"
+    }
+}
+
+if( -not $Configuration ) {
+    if( $env:CONFIGURATION ) { $Configuration = $env:CONFIGURATION }
+    else { $Configuration = 'Debug' }
+}
+
+dotnet build /p:Configuration=$Configuration /p:Version=$Version
