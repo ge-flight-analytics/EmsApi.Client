8ecb06fdce6f5578d561a02005486a21069c6965
Pedro Martínez <212788934@ge.com>
2025-02-11 16:44:49 -0600

    Fix function calls.

--- src/Client/V2/Access/AnalysisAccess.cs
+++ src/Client/V2/Access/AnalysisAccess.cs
@@ -15,14 +15,14 @@ namespace EmsApi.Client.V2.Access
             return AccessTaskResult( GetAnalysisGroupsAsync( context ) );
         }
 
-        public virtual Task<AnalysisGroup> GetAnalysisGroupAsync( CallContext context = null )
+        public virtual Task<AnalysisGroup> GetAnalysisGroupAsync( string groupId, CallContext context = null )
         {
-            return CallApiTask( api => api.GetAnalysisGroups( context: context ).ContinueWith( t => t.Result ) );
+            return CallApiTask( api => api.GetAnalysisGroup( groupId, context: context ).ContinueWith( t => t.Result ) );
 
         }
-        public virtual AnalysisGroup GetAnalysisGroup( CallContext context = null )
+        public virtual AnalysisGroup GetAnalysisGroup( string groupId, CallContext context = null )
         {
-            return AccessTaskResult( GetAnalysisGroupAsync( context ) );
+            return AccessTaskResult( GetAnalysisGroupAsync( groupId, context ) );
         }
 
         public virtual Task<Analysis> GetAnalysisAsync( string groupId, string analysisName, CallContext context = null )
